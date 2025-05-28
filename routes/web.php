d31cb2317983dfd2c97de01132b44c739fb10823
mcg <mcg>
2025-05-28 21:30:02 +0300

    delete view

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,6 @@
 use App\Jobs\ExampleJob;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::get('/test-job', function () {
     ExampleJob::dispatch('Merhaba RabbitMQ & Horizon!');
     return 'Job kuyruğa atıldı.';
