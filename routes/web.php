a51fcae1fafefe46c8d27840cb2d71fd5c8ce2d5
mcg <mcg>
2025-05-28 21:19:57 +0300

    completed

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Jobs\ExampleJob;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/test-job', function () {
+    ExampleJob::dispatch('Merhaba RabbitMQ & Horizon!');
+    return 'Job kuyruğa atıldı.';
 });
